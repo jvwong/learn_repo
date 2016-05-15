b12286912ea3246d13a2d05f1d56ffb7441188db
Jeffrey Wong <jvwong@outlook.com>
2016-05-14 20:11:42 -0400

    added JSONField for case references and summary

--- learn/cases/models.py
+++ learn/cases/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from core.models import TimeStampedModel
-from django.contrib.postgres.fields import ArrayField, IntegerRangeField
+from django.contrib.postgres.fields import ArrayField, IntegerRangeField, JSONField
 from django.core.urlresolvers import reverse
+
 
 class Article(TimeStampedModel):
     title = models.CharField(max_length=250)
@@ -29,21 +30,14 @@ class Case(TimeStampedModel):
     title = models.CharField(max_length=250, unique=True)
     subtitle = models.CharField(max_length=250, blank=True)
     article = models.OneToOneField(Article)
-    summary = ArrayField(
-        models.CharField(max_length=250, blank=True),
-        size=10,
-        blank=True
-    )
+    quick_summary = JSONField(blank=True, null=True)
     author_profile = models.TextField(blank=True)
     context = models.TextField(blank=True)
     question = models.TextField(blank=True)
     researcher_goals = models.TextField(blank=True)
     approach = models.TextField(blank=True)
-    references = ArrayField(
-        models.CharField(max_length=500, blank=True),
-        size=20,
-        blank=True
-    )
+    summary = models.TextField(blank=True)
+    references = JSONField(blank=True, null=True)
     quote = models.TextField(blank=True)
     figure = models.ImageField(upload_to='images/', blank=True)
     figure_legend = models.TextField(blank=True)
